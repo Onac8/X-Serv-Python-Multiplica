53a79991dc29b3b0ece83b5a7a42eec7437c40c8
Onac8 <canopicazojonathan@gmail.com>
2017-01-30 12:56:24 +0100

    Tabla de Multiplicar. Por Jonathan Cano Picazo.
    Estudiante de Tecnologías. Alias Onac8.

---
+++ multiplica.py
@@ -0,0 +1,7 @@
+#THIS IS A COMMENTARY
+
+for i in range(1,11):
+    print ("TABLA DEL " + str(i) + "\n-----------")
+    for x in range (1,11):
+        b = i * x
+        print (str(i) + " por " + str(x) + " es " + str(b))
